de30dffe1319cc3777fdd0a6d2549aa33d620107
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2009-10-16 13:49:49 +0000

    Adding pyutilib.virtualenv

---
+++ README.txt
@@ -0,0 +1,60 @@
+==========================
+pyutilib.virtualenv README
+==========================
+
+The pyutilib.virtualenv Python package includes a script for 
+creating virtualenv installers that automate the installation of 
+other Python packages.
+
+
+INSTALLATION
+------------
+
+See the INSTALL.txt file.
+
+
+LICENSE
+-------
+
+See the LICENSE.txt file.
+
+
+GETTING STARTED
+---------------
+
+Directories
+
+* pyutilib - The root directory for PyUtilib's Python source code
+
+
+Documentation and Bug Tracking
+
+* The PyUtilib Trac wiki supports documentation, issue tracking, and 
+  browsing of the PyUtilib subversion repository:
+
+  * https://software.sandia.gov/trac/pyutilib
+
+Authors
+
+* See the AUTHORS.txt file.
+
+Project Managers
+
+* William E. Hart, wehart@sandia.gov
+
+Mailing List
+
+* PyUtilib is managed with the Acro Project. A separate checkins mailing
+   list is managed for PyUtilib, but otherwise the main Acro mailing lists
+   are used to manage the development of this software:
+
+    - acro-developers@software.sandia.gov
+    - acro-users@software.sandia.gov
+
+
+THIRD PARTY SOFTARE
+-------------------
+
+The pyutilib.virtualenv package depends on the following Python packages:
+    - virtualenv
+
---
+++ setup.py
@@ -0,0 +1,50 @@
+#  _________________________________________________________________________
+#
+#  PyUtilib: A Python utility library.
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the PyUtilib README.txt file.
+#  _________________________________________________________________________
+
+"""
+Setup for pyutilib.virtualenv package
+"""
+
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(name="pyutilib.virtualenv",
+    version=1.0,
+    maintainer='William E. Hart',
+    maintainer_email='wehart@sandia.gov',
+    url = 'https://software.sandia.gov/trac/pyutilib',
+    license = 'BSD',
+    platforms = ["any"],
+    description = 'Utilities for building custom virtualenv bootstrap scripts.',
+    long_description = read('README.txt'),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Unix Shell',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+      packages=['pyutilib', 'pyutilib.ply'],
+      keywords=['utility'],
+      namespace_packages=['pyutilib']
+      )
+
