e63472feb2bff584cdae07b118ec4adb40095cca
wehart <wehart@570ccb8d-5833-0410-9ce8-cbedd0da42eb>
2013-02-17 05:50:47 +0000

    Change the order so setuptools or distribute come before pip

--- pyutilib/virtualenv/vpy_install.py
+++ pyutilib/virtualenv/vpy_install.py
@@ -4402,11 +4402,11 @@ class Installer(object):
         #
         if not options.preinstall and not (options.trunk or options.release):
             self.sw_packages.insert( 0, Repository('virtualenv', pypi='virtualenv') )
+            self.sw_packages.insert( 0, Repository('pip', pypi='pip') )
             if sys.version_info[:2] < (3,0):
                 self.sw_packages.insert( 0, Repository('setuptools', pypi='setuptools') )
             else:
                 self.sw_packages.insert( 0, Repository('distribute', pypi='distribute') )
-            self.sw_packages.insert( 0, Repository('pip', pypi='pip') )
             #
             # Configure the package versions, for offline installs
             #
@@ -4440,11 +4440,11 @@ class Installer(object):
         # Get source packages
         #
         self.sw_packages.insert( 0, Repository('virtualenv', pypi='virtualenv') )
+        self.sw_packages.insert( 0, Repository('pip', pypi='pip') )
         if sys.version_info[:2] < (3,0):
             self.sw_packages.insert( 0, Repository('setuptools', pypi='setuptools') )
         else:
             self.sw_packages.insert( 0, Repository('distribute', pypi='distribute') )
-        self.sw_packages.insert( 0, Repository('pip', pypi='pip') )
         if options.preinstall:
             #
             # When preinstalling, add the setuptools package to the installation list
